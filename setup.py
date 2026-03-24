df6875ade1d8b2826bd53c62ee615a31331271e3
inputupdate <66434800+inputupdate@users.noreply.github.com>
2026-03-24 10:32:07 -0500

    Add setup.py for Vibe package configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vibe",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Vibe transcription/translation package",
+    author="Lithor",
+    author_email="roy.champion@.com",
+)
